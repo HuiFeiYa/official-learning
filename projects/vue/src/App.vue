84b409e54a14c1fac99cc8c1d0c70ffde2caddc7
guanwanxiao <guanwanxiao@mbse.com.cn>
2026-03-23 16:40:04 +0800

    feat: 初始化文档学习仓库

---
+++ projects/vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="page">
+    <h1>Vue 官方文档学习111</h1>
+    <p>项目目录：projects/vue</p>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.page {
+  font-family: system-ui, -apple-system, Segoe UI, Roboto, Arial, sans-serif;
+  padding: 24px;
+}
+</style>
